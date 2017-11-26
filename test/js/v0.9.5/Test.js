de971d94562eae469acec0cf3fe0d88f07ed3ffe
Naresh28Babu <naresh28babu@gmail.com>
2017-11-26 12:30:04 +0530

    v 0.9.6

---
+++ test/js/v0.9.5/Test.js
@@ -0,0 +1,233 @@
+const TestCases ={};
+
+TestCases.getAllRecord = function(callBack)
+{
+		ZOHO.CRM.API.getAllRecords({Entity:"Leads"})
+			.then(function(data){
+				if(data && typeof(data) === 'object' && data instanceof Array )
+				{
+					callBack(true);
+				}
+				else
+				{
+					callBack(false);
+				}
+			})
+};
+TestCases.insertRecord=function(module,recordData,callBack)
+{
+			ZOHO.CRM.API.insertRecord({Entity:module,APIData:recordData})
+			.then(function(data){
+				if(data && typeof(data) === 'object' && data[0] && data[0].code==='SUCCESS'){
+					leadID = data[0].details.id;
+					callBack(true,leadID);
+				}
+				else
+				{
+					callBack(false);
+				}
+			})
+};
+TestCases.deleteRecord=function(module,recordID,callBack){
+		  if(!recordID){
+				callBack(false);
+		  }
+		  else{
+
+			  ZOHO.CRM.API.deleteRecord({Entity:module,RecordID: recordID})
+				.then(function(data){
+					if(data && typeof(data) === 'object' && data[0] && data[0].code==='SUCCESS'){
+						callBack(true);
+					}
+					else
+					{
+						callBack(false);
+					}
+				})	
+		  }
+		  
+};
+TestCases.getRecord = function(module,recordID,recordData,callBack){
+		  if(!recordID){
+				callBack(false);
+		  }
+		  else{
+		  	ZOHO.CRM.API.getRecord({Entity:module,RecordID:recordID})
+			.then(function(data){
+				if(data && typeof(data) === 'object' )
+				{
+					for(field in recordData){
+
+						if(recordData[field] == data[field]){
+							continue
+						}
+						callBack(false);			
+					}
+					callBack(true);
+				}
+				else{
+						callBack(false);
+				}
+
+			})
+		  }
+};
+TestCases.getUser = function(userID,callBack){
+		  if(!userID)
+		  {
+		  	ZOHO.CRM.API.getAllUsers({Type:"AllUsers"})
+				.then(function(data){
+				    if(data && data.length && data.length > 0 ){
+						callBack(true,data[0].id);
+				    }
+				    else{
+				    	callBack(false);
+				    }
+				});
+		  }
+		  else
+		  {
+			ZOHO.CRM.API.getUser({ID:userID})
+				.then(function(data){
+					if(data && data.id == userID){
+						callBack(true);
+					}
+					else
+					{
+						callBack(false);
+					}
+				})
+		  }
+};
+TestCases.getOrgVariable = function(variableName,callBack)
+{
+	ZOHO.CRM.CONFIG.getOrgVariable(variableName).then(function(data){
+		if(data || data.Success || data.Error)
+		{
+			callBack(true);
+		}
+		else
+			{
+				callBack(false);
+			}
+	});
+}
+TestCases.checkHttpRequest = function(url,callBack){
+	var request ={
+		url : url
+	}
+	ZOHO.CRM.HTTP.get(request).then(function(httpData){
+		var httpData = httpData;
+    	ZOHO.CRM.HTTP.get(request).then(function(httpsData){
+    		var httpsData = httpsData;
+    		if(httpData && httpsData)
+    		{
+    			callBack(true);
+    		}
+    		else
+    		{
+    			callBack(false)
+    		}
+    	});
+	});
+}
+TestCases.getFields = function(module,callBack){
+	ZOHO.CRM.META.API.getFields({Entity:module}).then(function(result){
+		if(result)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
+TestCases.getModules = function(module,callBack){
+	ZOHO.CRM.META.API.getModules({Entity:module}).then(function(result){
+		if(result)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
+TestCases.getAssignmentRules = function(module,callBack){
+	ZOHO.CRM.META.API.getAssignmentRules({Entity:module}).then(function(data){
+		if(data)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
+TestCases.getCurrentUser = function(callBack){
+	ZOHO.CRM.CONFIG.getCurrentUser().then(function(data){
+		if(data)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
+TestCases.getOrgInfo = function(callBack)
+{
+	ZOHO.CRM.CONFIG.getOrgInfo().then(function(data){
+		if(data)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
+TestCases.Search = function(module,type,query,callBack){
+	ZOHO.CRM.API.searchRecord({Entity:module,Type:type,Query:query})
+	.then(function(data){
+	    if(data)
+	   	{
+	   		callBack(true);
+	   	}
+	   	else
+	   	{
+	   		callBack(false);
+	   	}
+	})
+}
+TestCases.invokeConnectorWithoutDynamic = function(apiname,data,callBack){
+	ZOHO.CRM.CONNECTOR.invokeAPI(apiname,{})
+	.then(function(data){
+		if(data)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
+TestCases.invokeConnectorwithDynamic = function(apiname,data,callBack){
+	ZOHO.CRM.CONNECTOR.invokeAPI(apiname,data)
+	.then(function(data){
+		if(data)
+		{
+			callBack(true);
+		}
+		else
+		{
+			callBack(false);
+		}
+	});
+}
---
+++ test/js/v0.9.5/TestFlow.js
@@ -0,0 +1,151 @@
+describe("SDK Testing", function() {
+	var TestSpec={
+		recordID:undefined,
+		recordData : {
+		        "Company": "Zylker",
+		        "Last_Name": "Peterson"
+		  },
+		userID:undefined,
+		orgVariable:"unittest0.token",
+		url: "http://mockbin.org/bin/9b6c1e8a-ebf8-4fc8-a729-46175eb2c05c",
+		connector:"unittest0.unittest.getfiles",
+		fileId : "0B-EvY2Wt1MdxM1NxQjRxcG9GbXc",
+		connectorFile : "unittest0.unittest.getfile"
+	};
+	beforeAll(function(done) 
+	{
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
+		ZOHO.embeddedApp.on("DialerActive",function(){
+		console.log("Dialer Activated");
+		})
+		ZOHO.embeddedApp.on("Dial",function(number){
+				console.log(number);
+				console.log("Number Dialed");
+		})
+		ZOHO.embeddedApp.init()
+		.then(function()
+		{
+			done();
+		});
+	});
+
+  	/*
+	 * Insert a new Record into the system
+	 */
+	it("Insert Lead", function(done)
+	{
+		TestCases.insertRecord("Leads",TestSpec.recordData,function(result,recordID){
+			TestSpec.recordID = recordID;
+			expect(result).toBe(true);
+			done();
+		});
+	});
+	/*
+	 * Getch The Lead using the RecordID and verify its data
+	 */
+	it("get Lead", function(done)
+	{
+		TestCases.getRecord("Leads",TestSpec.recordID,TestSpec.recordData,function(result){
+			expect(result).toBe(true);
+			done();
+		});
+	});
+
+
+	it("getAll with Data", function(done)
+	{
+		  TestCases.getAllRecord(function(result){
+			expect(result).toBe(true);
+		  	done();
+		  });
+  	});
+	it("Delete Lead", function(done)
+	{
+		TestCases.deleteRecord("Leads",TestSpec.recordID,function(result){
+			expect(result).toBe(true);
+			done();
+		});
+  	});
+  	it("getAll Users", function(done)
+	{
+		  TestCases.getUser(undefined,function(result,userID){
+		  	TestSpec.userID = userID;
+			expect(result).toBe(true);
+		  	done();
+		  });
+  	});
+  	it("getUserByID", function(done)
+	{
+		if(!TestSpec.userID){
+			expect(true).toBe(false);
+		}
+		else
+		{
+			TestCases.getUser(TestSpec.userID,function(result){
+				expect(result).toBe(true);
+		  		done();
+		  	});	
+		}
+		  
+  	});
+  	it("getOrgVariable",function(done){
+  		TestCases.getOrgVariable(TestSpec.orgVariable,function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("checkHtttpRequst",function(done){
+  		TestCases.checkHttpRequest(TestSpec.url,function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("getFields",function(done){
+  		TestCases.getFields("Leads",function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("getModules",function(done){
+  		TestCases.getModules("Leads",function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("getAssignmentRules",function(done){
+  		TestCases.getAssignmentRules("Leads",function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("getCurrentUser",function(done){
+  		TestCases.getCurrentUser(function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("getOrgInfo",function(done){
+  		TestCases.getOrgInfo(function(result){
+  			expect(result).toBe(true);
+  			done();
+  		})
+  	});
+  	it("searchRecord",function(done){
+  		TestCases.Search("Leads","email","uk@gmail.com",function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("invokeConnectorWithoutDynamic",function(done){
+  		TestCases.invokeConnectorWithoutDynamic(TestSpec.connector,{},function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+  	it("invokeConnectorWithoutDynamicValue",function(done){
+  		TestCases.invokeConnectorwithDynamic(TestSpec.connectorFile,{fileId:TestSpec.fileId},function(result){
+  			expect(result).toBe(true);
+  			done();
+  		});
+  	});
+});
